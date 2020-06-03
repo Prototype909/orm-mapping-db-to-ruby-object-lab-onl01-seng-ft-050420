79a7a59cc1072301a48eeae5c24b6172e4896257
Steve Anderson <prototype909@msn.com>
2020-06-03 22:11:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,4 +70,13 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL 
+    SELECT *
+    from students
+    WHERE grade < 12
+    SQL
+    DB[:conn.execute(slq)
+  end 
 end
