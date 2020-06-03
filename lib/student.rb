bbac2bd32e05dc2cdcb0ecc8663eb6a107b4e699
Steve Anderson <prototype909@msn.com>
2020-06-03 22:27:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,4 +105,15 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql,number).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
