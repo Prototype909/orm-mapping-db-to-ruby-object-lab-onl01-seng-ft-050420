7cd737e050bf799aa6bc50ec1d0218415f9a5285
Steve Anderson <prototype909@msn.com>
2020-06-03 22:02:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,12 @@ class Student
     SELECT *
     FROM students
     WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+      end.first
+    end
   end
 
   def save
